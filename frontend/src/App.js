88ab1291b51730c89930908c4f67e2d9d05f4fcb
Kevin Jin <kevinjin@Kevins-MacBook-Pro.local>
2021-10-23 17:19:19 -0500

    Added new branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import Router from './Router'
 import {BrowserRouter} from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './theme'
+import { CssBaseline } from '@material-ui/core'
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
+    
   <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <CssBaseline/>
         <Router/>
       </ThemeProvider>
   </BrowserRouter>
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -6,28 +6,38 @@ import { pink } from '@material-ui/core/colors'
  */
 const theme = createTheme({
     typography: {
-      useNextVariants: true,
+        fontFamily: [
+            'Nunito',
+            // 'regular',
+            // 'bold'
+          ].join(','),
+        useNextVariants: true,
     },
     palette: {
-      primary: {
-      light: '#5c67a3',
-      main: '#3f4771',
-      dark: '#2e355b',
-      contrastText: '#fff',
-      action: {
-        disabledBackground: 'rgba(0,0,0,0.25)',
-        disabled: 'rgba(0,0,0,0.25)'
-      }
+        background: {
+            default: '#fefbef'
+        },
+        primary: {
+            
+
+            light: '#5c67a3',
+            main: '#3f4771',
+            dark: '#2e355b',
+            contrastText: '#fff',
+            action: {
+                disabledBackground: 'rgba(0,0,0,0.25)',
+                disabled: 'rgba(0,0,0,0.25)'
+            }
     },
     secondary: {
-      light: '#ff79b0',
-      main: '#ff4081',
-      dark: '#c60055',
-      contrastText: '#000',
+        light: '#ff79b0',
+        main: '#ff4081',
+        dark: '#c60055',
+        contrastText: '#000',
     },
-      openTitle: '#3f4771',
-      protectedTitle: pink['400'],
-      type: 'light'
+        openTitle: '#3f4771',
+        protectedTitle: pink['400'],
+        type: 'light'
     },
   })
 
