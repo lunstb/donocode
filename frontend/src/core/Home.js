795d675549811f49a41b4d18ee066765bed98ebe
Kevin Jin <kevinjin@Kevins-MacBook-Pro.local>
2021-10-23 21:51:13 -0500

    Home ui

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -19,11 +19,14 @@ const useStyles = makeStyles((theme) => ({
             textDecoration: 'none',
             color: '#51323C'
         },
-        color: '#51323C'
+        color: '#51323C',
+        margin: "0 20px"
 
     },
     logo: {
-        fontWeight: 'bold',
+        padding: '0 0 0 10vh',
+        margin: "0 20px 0 0",
+        fontWeight: '900!important',
         color: '#d9af00',
         '&:link': {
             textDecoration: 'none',
@@ -37,17 +40,24 @@ const useStyles = makeStyles((theme) => ({
     
     toolbar: {
         backgroundColor: '#fefbef',
+        padding: '0'
     },
     appBar: {
-        boxShadow: 'none'
+        boxShadow: 'none',
+        marginTop: '20px'
     },
     heroContent: {
         
         lineHeight: '1.3em',
-        paddingBottom: '40px'
+        paddingBottom: '7vh',
+        ['@media(max-width: 950px)'] : {
+            
+            paddingBottom: '5vh'
+          },
+        color: '#51323C'
     },
     login: {
-        padding: '10px 25px',
+        padding: '15px 25px',
         boxShadow: 'none',
         textTransform: 'none',
         borderRadius: '5px',
@@ -60,7 +70,11 @@ const useStyles = makeStyles((theme) => ({
         }
     },
     hero: {
-        padding: '30vh 7%'
+        padding: '20vh 7%',
+        ['@media(max-width: 950px)'] : {
+            
+            textAlign: 'center'
+          },
     },
     createText: {
         '&:link': {
@@ -74,11 +88,18 @@ const useStyles = makeStyles((theme) => ({
         textDecoration: 'none',
     },
     image: {
-        position: 'absolute',
-        right: '10px',
-        bottom: '20%'
-        
-    }
+        ['@media(min-width: 950px)'] : {
+            position: 'absolute',
+            width: '40em'
+          },
+        ['@media(max-width: 950px)'] : {
+            
+            position: 'relative',
+            width: '25em'
+          },
+        right: '6%',
+        bottom: '20vh',
+    },
   }));
   
 
@@ -90,26 +111,26 @@ export default function Home(){
     <AppBar position="static" className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
             <Link className={clsx(classes.title, classes.logo)} to="/">
-                <Typography aria-label="Home" variant="h6">
-                    <b>DonoCode</b>
+                <Typography aria-label="Home" variant="h4">
+                    <b style={{fontWeight: '900'}}>DonoCode</b>
                 </Typography>
             </Link>
             <Link className={classes.title} to="/">
-                <Typography aria-label="Home" variant="h6">
-                    About
+                <Typography aria-label="Home" variant="h5">
+                    How it Works
                 </Typography>
             </Link>
             <Link className={classes.title} to="/">
-                <Typography aria-label="Home" variant="h6">
+                <Typography aria-label="Home" variant="h5">
                     FAQ
                 </Typography>
             </Link>
         </Toolbar>
     </AppBar>
     <div className={classes.hero}>
-        <Typography variant="h3" className={classes.heroContent}><b>Track your impact<br/>with DonoCode!</b></Typography>
+        <Typography variant="h2" className={classes.heroContent}><b style={{fontWeight: '800'}}>Track your impact<br/>with DonoCode!</b></Typography>
         <Button variant="contained" className={classes.login}><b>Create DonoCode</b></Button>
-        <img src={homeImage} width="800px" className={classes.image}/>
+        <img src={homeImage} className={classes.image}/>
     </div>
     
     </span>
