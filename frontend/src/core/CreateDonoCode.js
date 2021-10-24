fa514acf57caab6c1a5323c7ab43ce24cd748f35
Berke Lunstad <berke.k.lunstad@vanderbilt.edu>
2021-10-23 22:18:06 -0500

    more work on creating dono code

--- frontend/src/core/CreateDonoCode.js
+++ frontend/src/core/CreateDonoCode.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Helmet } from 'react-helmet';
-import { AppBar, Button, Container, Typography } from '@material-ui/core';
+import { AppBar, Button, Container, TextField, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { Toolbar } from '@material-ui/core';
 import clsx from 'clsx'
@@ -21,6 +21,31 @@ const useStyles = makeStyles((theme) => ({
         color: '#51323C'
 
     },
+    h1: {
+      fontSize: "5vh"
+    },
+    nextButton: {
+      backgroundColor: "#D9AF00",
+      fontSize: '4vh',
+      fontFamily: 'Nunito',
+      fontWeight: 'bold',
+      color: "#51323C",
+      textTransform: 'none',
+      borderRadius: "8px",
+      marginTop: "25px",
+      marginRight: "30px",
+      width: "10%",
+      '&:hover': {
+        background: '#C99F00',
+      },
+    },
+    darkColor: {
+      backgroundColor: "#51323C",
+      color: "#D9AF00",
+      '&:hover': {
+        background: '#41222C',
+      },
+    },
     logo: {
         fontWeight: 'bold',
         color: '#d9af00',
@@ -33,18 +58,105 @@ const useStyles = makeStyles((theme) => ({
             color: '#d9af00'
         },
     },
-    
+    pageContent: {
+      marginLeft: "50px"
+    },
+    numberField: {
+      marginRight: "20px",
+      width: "20vw",
+      marginBottom: "20px",
+      fontSize: '6vh',
+      [`& fieldset`]: {
+          borderRadius: "10px",
+      }
+    },
     toolbar: {
         backgroundColor: '#fefbef',
     },
     appBar: {
         boxShadow: 'none'
     },
+    textContent: {
+      marginBottom: '40px'
+    },
+    resize:{
+      fontSize:"6vh"
+    },
   }));
   
 
+
+function HowManyCodes(classes, currentPage, setCurrentPage){
+  return (
+    <div>
+      <Link className={classes.title} to="/">
+        <Typography aria-label="Home" variant="h6">
+          Back
+        </Typography>
+      </Link>
+      <div className={classes.textContent}>
+        <h1 className={classes.h1}>How many DonoCodes would you like to create?</h1>
+        <div>
+          <TextField id="standard-basic" label="type number here" variant="standard" className={classes.numberField} 
+            InputProps={{
+              classes: {
+                input: classes.resize,
+              },
+            }}
+          />
+        </div>
+      </div>
+      <div>
+        <Button onClick={()=>{setCurrentPage(currentPage+1)}}className={classes.nextButton} variant="contained" disableElevation>Next</Button>
+      </div>
+    </div>
+  )
+}
+
+function LinkToAccount(classes, currentPage, setCurrentPage, linkToAccount, setLinkToAccount){
+  return (
+    <div>
+      <Button onClick={()=>{setCurrentPage(currentPage-1)}}className={classes.backButton} disableElevation>Back</Button>
+      <div className={classes.textContent}>
+        <h1 className={classes.h1}>Would you like to link these DonoCodes to your account?</h1>
+        <p>For example, if you are a donation center planning to hand DonoCodes out to Donors, click no.</p>
+      </div>
+      <Button onClick={()=>{setLinkToAccount(true); setCurrentPage(currentPage+1)}}className={classes.nextButton} disableElevation>Yes</Button>
+      <Button onClick={()=>{setLinkToAccount(false); setCurrentPage(currentPage+1)}}className={clsx(classes.nextButton, classes.darkColor)} disableElevation>No</Button>
+    </div>
+  )
+}
+
+function AttachMessage(classes, currentPage, setCurrentPage){
+  return (
+    <div>
+      <Button onClick={()=>{setCurrentPage(currentPage-1)}}className={classes.backButton} disableElevation>Back</Button>
+      <h2>Add a personal message with your DonoCode</h2>
+      <p>If you choose to not put a personalized message, your recipients will receive a default message. </p>
+      
+    </div>
+  )
+}
+
 export default function CreateDonoCode(){
     const classes = useStyles()
+    const [currentPage, setCurrentPage] = useState(0);
+    const [linkToAccount, setLinkToAccount] = useState(false);
+
+    let page;
+    switch (currentPage) {
+      case 0:
+        page = HowManyCodes(classes, currentPage, setCurrentPage)
+        break;
+      case 1:
+        page = LinkToAccount(classes, currentPage, setCurrentPage, linkToAccount, setLinkToAccount)
+        break;
+      case 2:
+        page = AttachMessage(classes, currentPage, setCurrentPage)
+        break;
+      default:
+        break;
+    }
     
     return (
       <div>
@@ -62,12 +174,9 @@ export default function CreateDonoCode(){
                 </Link>
             </Toolbar>
         </AppBar>
-        <Link className={clsx(classes.title, classes.logo)} to="/">
-
-            <Typography aria-label="Home" variant="h6">
-                <b>Back</b>
-            </Typography>
-        </Link>
+        <div className={classes.pageContent}>
+          {page}
+        </div>
       </div>
     
     );
